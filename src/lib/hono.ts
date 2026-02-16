2f7e3d14993617ab4fe8c11fe847f6f45a5c96ef
Mian Mubbashir Elahi <136785142+mubbashiralgoace@users.noreply.github.com>
2026-02-16 17:59:26 +0500

    fixed

--- src/lib/hono.ts
+++ src/lib/hono.ts
@@ -2,4 +2,6 @@ import { hc } from 'hono/client';
 
 import type { AppType } from '@/app/api/[[...route]]/route';
 
-export const client = hc<AppType>(process.env.NEXT_PUBLIC_APP_BASE_URL!);
+const baseUrl = typeof window === 'undefined' ? process.env.NEXT_PUBLIC_APP_BASE_URL! : window.location.origin;
+
+export const client = hc<AppType>(baseUrl);
